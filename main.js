092c00efcbeafe467c26884542aba6d523cb8849
pecopong <pecopong90@gmail.com>
2019-12-31 11:49:15 +0900

    Add restart

--- main.js
+++ main.js
@@ -1,34 +1,63 @@
 const init = function(){
 
-    const cons = document.createElement('div')
-    cons.style['position'] = 'fixed'
-    cons.style['top'] = '0px'
-    cons.style['left'] = '0px'
-    cons.style['color'] = 'red'
-    cons.style['font-size'] = '40px'
-    document.body.appendChild(cons)
+    // Fullscreen
+    const makeFs = function(){
+        const fs = document.createElement('div')
+        fs.innerHTML = 'Tap to enter fullscreen'
+        fs.id = 'fs'
+        document.body.appendChild(fs)
+        fs.onclick = function(){
+            document.body.requestFullscreen()
+            fs.remove()
+        }
+    }
+    makeFs()
 
+    // DOM
     const btnStart = document.querySelector('#start')
     const board = document.querySelector('#board')
     const resultBoard = document.querySelector('#resultBoard')
     const resultSpeed = document.querySelector('#resultSpeed')
     const coindeck = document.querySelector('#coindeck')
+    const restartWrap = document.querySelector('#restartWrap')
+    const btnRestart = document.querySelector('#btnRestart')
     const page = document.querySelectorAll('.page')
     const btnCoin = document.querySelectorAll('.btnCoin')
     const btnSend = document.querySelectorAll('.btnSend')
 
-    btnStart.onclick = function(){
-        page[1].style['z-index'] = 1
-        stage()
-    }
-
     let isStart = false
     let timer
     let timestamp = 0
-    let val
+    let val //Target value
+    let coinImg = [] // coin image DOM
+    let count = 0 // coin count
+    let sum = 0 // value sum
+
+    // Initialize
+    const initStage = function(){
+        timestamp = 0
+        resultSpeed.innerHTML = ''
+        val = 0
+        board.innerHTML = ''
+        for(var i = 0; i < count; i ++){
+            coinImg[i].remove()
+            console.log(i)
+        }
+        count = 0
+        sum = 0
+        coinImg = []
+        resultBoard.innerHTML = ''
+    }
+
+    btnStart.onclick = function(){
+        page[1].style['z-index'] = 1
+        initStage()
+        stage()
+    }
+    
     const timecheck = function(){
         timestamp ++
-        resultSpeed.innerHTML = (timestamp * 0.1).toFixed(1) + 'msec'
+        resultSpeed.innerHTML = (timestamp * 0.01).toFixed(2) + 'msec'
     }
 
     const stage = function(){
@@ -48,7 +77,7 @@ const init = function(){
         let randCount
         randCount = Math.random() * 2000 + 2000
         
-        val = Math.floor(Math.random() * 10) * 500 + Math.floor(Math.random()*4) * 100
+        val = Math.floor(Math.random() * 10) * 500 + Math.floor(Math.random()*4) * 100 // Target value
 
         setTimeout(function(){
             board.innerHTML = val
@@ -70,18 +99,15 @@ const init = function(){
             result()
         }
     }
-
-    let coinImg = []
-    let count = 0
-    let sum = 0
     
     const coinStack = function(type){
-        count ++
+        
         coinImg[count] = document.createElement('img')
         coinImg[count].src = './data/coin_'+type+'.png'
-        coinImg[count].style['transform'] = 'translate('+30*Math.random()+'px, '+count * -25+'px)'
+        coinImg[count].style['transform'] = 'translate('+30*Math.random()+'px, '+count * -18+'px)'
         coindeck.append(coinImg[count])
         coinImg[count].className = 'coinImg500'
+        count ++
         sum += type
     }
 
@@ -93,11 +119,15 @@ const init = function(){
             resultBoard.style['color'] = 'rgb(255,0,0)'
         }
         clearInterval(timer)
+        isStart = false
+        restartWrap.style['display'] = 'initial'
     }
 
-    if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-            .register('/sw.js')
+    // Restart
+    btnRestart.onclick = function(){
+        initStage()
+        stage()
+        restartWrap.style['display'] = 'none'
     }
 }
 
